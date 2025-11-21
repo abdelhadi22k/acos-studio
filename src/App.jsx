47ec1a6daf694204108149bd0cb0b37809d50f53
abdelhadi22k <115563437+abdelhadi22k@users.noreply.github.com>
2025-11-21 10:05:43 -0800

    Update App.jsx 2
    add 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,14 @@ import BlogDetels from "./components/blog/BlogDetels";
 import SearchPage from "./page/SearchPage";
 import BlogPage from "./page/BlogPage";
 import NotFound from "./page/NotFound";
-=======
-import BlogDetels from './components/blog/BlogDetels';
-import SearchPage from "./page/SearchPage";
-import BlogPage from "./page/BlogPage";
 
 
 
->>>>>>> 4a1292ddb3e8c5c6aab458b8df7a0d8b53df0dc0
 
 const App = () => {
   return (
     <HeadProvider>
-<<<<<<< HEAD
+
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<MainPage />} />
@@ -28,16 +23,8 @@ const App = () => {
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
-=======
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-         <Route path="/Blog" element={<BlogPage />} />
-        <Route path="/Blog/:slug" element={<BlogDetels />} />
-        <Route path="/search" element={<SearchPage />} />
-      </Routes>
-    </BrowserRouter>
->>>>>>> 4a1292ddb3e8c5c6aab458b8df7a0d8b53df0dc0
+
+ 
     </HeadProvider>
   );
 };
